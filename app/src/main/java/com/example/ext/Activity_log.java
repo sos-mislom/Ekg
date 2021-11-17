0f0da1a66ec46a72366376d64598de4764c7be8d
sos-mislom <y4itchko@yandex.ru>
2021-11-17 12:30:50 +0500

    cosdksd[ldsf

--- app/src/main/java/com/example/ext/Activity_log.java
+++ app/src/main/java/com/example/ext/Activity_log.java
@@ -1,8 +1,11 @@
 package com.example.ext;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.security.NoSuchAlgorithmException;
 
-public class MainActivity extends AppCompatActivity {
+public class Activity_log extends AppCompatActivity {
     EditText usernameFromLay;
     EditText passwordFromLay;
     private StringBuilder strTree = new StringBuilder();
@@ -22,34 +25,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         usernameFromLay = (EditText) findViewById(R.id.login);
         passwordFromLay = (EditText) findViewById(R.id.password);
-        logInBtn = (Button) findViewById(R.id.login_button);
+        logInBtn = (Button) findViewById(R.id.enter);
 
 
-        //logTview = (TextView) findViewById(R.id.tvLog);
-        asyncEXT task = new asyncEXT();
-        task.execute();
+        logTview = (TextView) findViewById(R.id.auth);
+
+/*        asyncEXT task = new asyncEXT();
+        task.execute();*/
+
         logInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = usernameFromLay.getText().toString();
                 String password = passwordFromLay.getText().toString();
                 int Code_Of_Check_auth = AUTHCheck.checkLogin(username, password);
-                if (Code_Of_Check_auth == 200){ logTview.setText("success");
+                if (Code_Of_Check_auth == 200){ logTview.setText("Успех");
+                    Intent i = new Intent(Activity_log.this, Activity_after_log.class);
+                    i.putExtra("result", username+" "+password);
+                    startActivity(i);
                 }
-                else if (Code_Of_Check_auth == 103){logTview.setText("incorrect login");
+                else if (Code_Of_Check_auth == 103){logTview.setText("Неверный логин");
                 }
                 else if (Code_Of_Check_auth == 104){
-                    logTview.setText("incorrect password");
+                    logTview.setText("Неверный пароль");
                 }
             }
         });
     }
 
-    @SuppressLint("StaticFieldLeak")
+ /*   @SuppressLint("StaticFieldLeak")
     private class asyncEXT extends AsyncTask<Void, Void, Void> {
         private void UCH_YEAR() {
             try {
@@ -71,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
             logTview.setText(strTree);
             System.out.println(strTree);
         }
-    }
+    }*/
 
     public static class AUTHCheck {
         private static final int MIN_PASS_LENGHT = 10;
