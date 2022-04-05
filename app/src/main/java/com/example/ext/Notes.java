9a37c9a5685f7a4e476daa8e0aceaf0647b97a73
sos-mislom <60609474+sos-mislom@users.noreply.github.com>
2022-04-05 20:28:29 +0500

    change regex regular in Notes

--- app/src/main/java/com/example/ext/Notes.java
+++ app/src/main/java/com/example/ext/Notes.java
@@ -226,7 +226,7 @@ public class Notes extends AppCompatActivity {
                             break;
                     }
                     weight = Double.parseDouble(array.get(j).get(3));
-                    if(!array.get(j).get(2).matches("[a-zA-ZА-Яа-я]+")){
+                    if(array.get(j).get(2).matches("[0-9]")){
                         count++;
                         nominator += Integer.parseInt(array.get(j).get(2)) * weight;
                         denominator += weight;
